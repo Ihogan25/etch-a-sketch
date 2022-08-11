ab0da2464a50f6503221b1d66a78c31c320053c3
Ihogan25 <isaiah.hogan25@gmail.com>
2022-08-11 09:04:49 -0500

    configuring grid

---
+++ script.js
@@ -0,0 +1,7 @@
+const pixel = document.querySelectorAll('.pixel');
+const btnBlack = document.getElementById('black');
+
+
+pixel.forEach(addEventListener('mouseover', function(){
+    document.pixel.backgroundColor = 'black'
+}))
