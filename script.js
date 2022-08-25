6ca94025dd9ab8b52387948a9e2aab7272a7d451
Isaiah Hogan <isaiah.hogan25@gmail.com>
2022-08-25 18:07:31 -0500

    added pixel

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const pixel = document.querySelectorAll('.pixel');
-const btnBlack = document.getElementById('black');
+let board = document.querySelector('.board');
+let add = document.getElementById('add');
+
+add.addEventListener('click', function(){
+    let value = window.prompt('How Many Pixels Do you Want?');
+    for(let i = 0; i <= value ; i++) {
+    let pixel = document.createElement('div');
+    board.appendChild(pixel)
+    pixel.className = 'pixel';
+    console.log(pixel, value);
+    }
+})
+
+   
 
 
-pixel.forEach(addEventListener('mouseover', function(){
-    document.pixel.backgroundColor = 'black'
-}))
